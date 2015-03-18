e404423331549e27f142a57beddbb0b7de97f85b
nampnq <nampnq@gmail.com>
2015-03-18 13:41:39 +0700

    Migrate db and complete auth

--- assets.py
+++ assets.py
@@ -1,16 +1,17 @@
 from flask.ext.assets import Environment, Bundle
 
 css_all = Bundle(
-    'lib/bootstrap/bootstrap.css',
-    'lib/bootstrap-select/bootstrap-select.css',
-    'lib/font-awesome/font-awesome.css',
+    'lib/bootstrap/css/bootstrap.css',
+    'lib/bootstrap-select/css/bootstrap-select.css',
+    'lib/font-awesome/css/font-awesome.css',
     'css/admin/*.css',
     'css/*.css',
     output='application.css'
 )
 js_all = Bundle(
-    'js/jquery.js',
+    'js/jquery.min.js',
     output='application.js')
+
 
 def register_app(app):
     assets = Environment(app)
--- manage.py
+++ manage.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import os
-import sys
-import subprocess
-from flask.ext.script import Manager, Shell, Server
-from flask.ext.migrate import MigrateCommand
+from flask.ext.script import Manager
+from flask.ext.migrate import MigrateCommand, Migrate
+from app import app, db
+# noinspection PyUnresolvedReferences
+import models
 
 manager = Manager(app)
+migrate = Migrate(app, db)
 
 manager.add_command('db', MigrateCommand)
 
