6af1a6983166470543a5e047552576b66fb580c7
nampnq <nampnq@gmail.com>
2015-03-20 10:25:12 +0700

    Update model

--- models.py
+++ models.py
@@ -1,74 +1,34 @@
-import os
-from flask import current_app
 from datetime import datetime
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Text
-from sqlalchemy.schema import Index, UniqueConstraint
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
 
 from app import db
 from db.types.json import JSONEncodedDict
 
 
-class App(db.Model):
-    __tablename__ = 'app'
-    __table_args__ = (
-        Index('idx_app_repository_id', 'repository_id'),
-    )
+class Project(db.Model):
+    __tablename__ = 'project'
 
     id = Column(Integer, primary_key=True)
-    repository_id = Column(Integer,
-                           ForeignKey('repository.id', ondelete="CASCADE"),
-                           nullable=False)
     name = Column(String(200), nullable=False, unique=True)
-    provider = Column(String(64))
-    data = Column(JSONEncodedDict)
-    date_created = Column(DateTime, default=datetime.utcnow, nullable=False)
+    repository_data = Column(JSONEncodedDict)
+    project_data = Column(JSONEncodedDict)
+    created_date = Column(DateTime, default=datetime.utcnow, nullable=False)
 
     @property
     def checks(self):
-        return self.data.get('checks', [])
+        return self.project_data.get('checks', [])
 
     @property
     def notifiers(self):
-        return self.data.get('notifiers', [])
+        return self.project_data.get('notifiers', [])
 
     @property
     def provider_config(self):
-        return self.data.get('provider_config', {})
+        return self.project_data.get('provider_config', {})
 
-
-LOG_CHUNK_SIZE = 4096
-
-
-class LogChunk(db.Model):
-    __tablename__ = 'logchunk'
-    __table_args__ = (
-        Index('idx_logchunk_task_id', 'task_id'),
-        UniqueConstraint('task_id', 'offset', name='unq_logchunk_source_offset'),
-    )
-
-    id = Column(Integer, primary_key=True)
-    task_id = Column(Integer, ForeignKey('task.id', ondelete="CASCADE"), nullable=False)
-    # offset is sum(c.size for c in chunks_before_this)
-    offset = Column(Integer, nullable=False)
-    # size is len(text)
-    size = Column(Integer, nullable=False)
-    text = Column(Text, nullable=False)
-    date_created = Column(DateTime, default=datetime.utcnow, nullable=False)
-
-
-class Repository(db.Model):
-    __tablename__ = 'repository'
-
-    id = Column(Integer, primary_key=True)
-    url = Column(String(200), nullable=False, unique=True)
-    vcs = Column(String(64), nullable=False)
-    data = Column(JSONEncodedDict)
-    date_created = Column(DateTime, default=datetime.utcnow, nullable=False)
-
-    def get_path(self):
-        return os.path.join(
-            current_app.config['WORKSPACE_ROOT'], 'fad-repo-{}'.format(self.id)
-        )
+    @property
+    def stages(self):
+        return self.project_data.get('stages', [])
 
 
 class TaskName(object):
@@ -115,29 +75,20 @@ STATUS_LABELS_REV = {
 
 class Task(db.Model):
     __tablename__ = 'task'
-    __table_args__ = (
-        Index('idx_task_app_id', 'app_id'),
-        Index('idx_task_user_id', 'user_id'),
-        UniqueConstraint('app_id', 'environment', 'number', name='unq_task_number'),
-    )
 
     id = Column(Integer, primary_key=True)
-    app_id = Column(Integer, ForeignKey('app.id', ondelete="CASCADE"),
-                    nullable=False)
-    number = Column(Integer, nullable=False)
+    project_id = Column(Integer, ForeignKey('project.id', ondelete="CASCADE"),
+                        nullable=False)
     user_id = Column(Integer, ForeignKey('user.id', ondelete="CASCADE"),
                      nullable=False)
     name = Column(String(128), nullable=False, default=TaskName.deploy)
-    ref = Column(String(128), nullable=False)
-    sha = Column(String(40))
-    environment = Column(String(64), nullable=False, default='production')
-    provider = Column(String(64), nullable=False)
+    sha = Column(String(128))
+    stage = Column(String(64), nullable=False)
     status = Column(Integer, nullable=False)
     data = Column(JSONEncodedDict)
-    date_created = Column(DateTime, default=datetime.utcnow, nullable=False)
-    # represents the start of the task (or the last time it was attempted)
-    date_started = Column(DateTime)
-    date_finished = Column(DateTime)
+    created_date = Column(DateTime, default=datetime.utcnow, nullable=False)
+    started_date = Column(DateTime)
+    finished_date = Column(DateTime)
 
     @property
     def checks(self):
@@ -153,7 +104,7 @@ class Task(db.Model):
 
     @property
     def status_label(self):
-        return STATUS_LABELS.get(self.status, 'unknown')
+        return STATUS_LABELS.get(int(self.status), 'unknown')
 
     @property
     def duration(self):
@@ -161,10 +112,44 @@ class Task(db.Model):
             return
         return float('%.2f' % (self.date_finished - self.date_started).total_seconds())
 
+    @property
+    def log(self):
+        return self.data.get('log', '')
+
+
+class UserRole(object):
+    admin = 0
+    developer = 1
+    viewer = 2
+
+    @classmethod
+    def get_label(cls, status):
+        return status
+
+    @classmethod
+    def label_to_id(cls, label):
+        return getattr(cls, label)
+
+UserRole_LABELS = {
+    UserRole.admin: 'Admin',
+    UserRole.developer: 'Developer',
+    UserRole.viewer: 'Viewer'
+}
+
+UserRole_LABELS_REV = {
+    v: k for k, v in STATUS_LABELS.items()
+}
+
 
 class User(db.Model):
     __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(200), nullable=False, unique=True)
+    email = Column(String(200), nullable=False, unique=True)
+    role = Column(Integer, nullable=False)
     date_created = Column(DateTime, default=datetime.utcnow, nullable=False)
+
+    @property
+    def role_label(self):
+        return UserRole_LABELS.get(int(self.role), 'Unknown')
